4345e72759af76447fc62336a5af19aee6ce2dce
Jones_lund <42931029+Coachmeister@users.noreply.github.com>
2020-04-15 13:11:41 +0200

    A bunch of zombies
    "small" zombie type

--- Warcell/smallEnemy/src/main/java/warcell/smallenemy/EnemyPlugin.java
+++ Warcell/smallEnemy/src/main/java/warcell/smallenemy/EnemyPlugin.java
@@ -13,25 +13,37 @@ import warcell.common.services.IGamePluginService;
 import warcell.common.utils.Vector2D;
 
 public class EnemyPlugin implements IGamePluginService {
+
     private String enemyID;
     private final String walkAnimationPath = "WalkingAnimation.png";
     private final int walkAnimationFrameColumns = 3;
     private final int walkAnimationFrameRows = 5;
-    
+    private final int AMOUNTOFENEMIES;
+
     public EnemyPlugin() {
+        AMOUNTOFENEMIES = 5;
+    }
+
+    public EnemyPlugin(int amountOfEnemies) {
+        AMOUNTOFENEMIES = amountOfEnemies;
     }
 
     @Override
     public void start(GameData gameData, World world) {
         // Add entities to the world
-        Entity enemy = createSmallZombie(gameData);
-        enemyID = world.addEntity(enemy);
-        
+        if (world == null || gameData == null) {
+            throw new IllegalArgumentException("World or gamedata is null");
+        }
+        for (int i = 0; i < AMOUNTOFENEMIES; i++) {
+
+            Entity enemy = createSmallZombie(gameData);
+            enemyID = world.addEntity(enemy);
+        }
     }
 
     private Entity createSmallZombie(GameData gameData) {
         Entity enemyZombie = new Enemy();
-        
+
         float deacceleration = 20;
         float acceleration = 400;
         float maxSpeed = 300;
@@ -40,11 +52,11 @@ public class EnemyPlugin implements IGamePluginService {
         float y = gameData.getDisplayHeight() / 3;
         float radians = 3.1415f / 2;
         enemyZombie.add(new LifePart(3));
-       // enemyZombie.setRadius(4);
+        // enemyZombie.setRadius(4);
         enemyZombie.add(new MovingPart(deacceleration, acceleration, maxSpeed, rotationSpeed));
         enemyZombie.add(new PositionPart(x, y, radians));
         enemyZombie.add(new AnimationTexturePart(new Vector2D(x, y), walkAnimationPath, walkAnimationFrameColumns, walkAnimationFrameRows, 0.25f));
-        
+
         return enemyZombie;
     }
 
