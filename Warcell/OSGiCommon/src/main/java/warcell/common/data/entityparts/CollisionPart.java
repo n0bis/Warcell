161a91fce1d2e91cbd95742afb7c124ecf89e9bb
Jones_lund <42931029+Coachmeister@users.noreply.github.com>
2020-04-28 10:49:34 +0200

    dfs

--- Warcell/OSGiCommon/src/main/java/warcell/common/data/entityparts/CollisionPart.java
+++ Warcell/OSGiCommon/src/main/java/warcell/common/data/entityparts/CollisionPart.java
@@ -1,14 +1,89 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package warcell.common.data.entityparts;
+
+import warcell.common.data.Entity;
+import warcell.common.data.GameData;
+import warcell.common.data.World;
 
 /**
  *
  * @author Jonas
  */
-public class CollisionPart {
+public class CollisionPart implements EntityPart {
+    
+    private boolean canCollide;
+    private boolean isHitEntity;
+    private boolean isHitGameMap;
+    private float timeSinceLastCollision;
+    private float minTimeBetweenCollision;
+
+    public CollisionPart() {
+    }
+
+    public CollisionPart(boolean canCollide, float minTimeBetweenCollision) {
+        this.canCollide = canCollide;
+        this.minTimeBetweenCollision = minTimeBetweenCollision;
+        this.isHitEntity = false;
+        this.isHitGameMap = false;
+        this.timeSinceLastCollision = 0;
+    }
+
+    @Override
+    public void process(GameData gameData, Entity entity) {
+        PositionPart positionpart = entity.getPart(PositionPart.class);
+        
+        
+         if ((this.isHitEntity() || this.isHitGameMap()) && positionpart != null){
+             
+             
+         }
+    }
+
+    
+    
+    
+    
+    public boolean isCanCollide() {
+        return canCollide;
+    }
+
+    public void setCanCollide(boolean canCollide) {
+        this.canCollide = canCollide;
+    }
+
+    public boolean isHitEntity() {
+        return isHitEntity;
+    }
+
+    public void setIsHitEntity(boolean isHitEntity) {
+        this.isHitEntity = isHitEntity;
+    }
+
+    public boolean isHitGameMap() {
+        return isHitGameMap;
+    }
+
+    public void setIsHitGameMap(boolean isHitGameMap) {
+        this.isHitGameMap = isHitGameMap;
+    }
+
+    public float getTimeSinceLastCollision() {
+        return timeSinceLastCollision;
+    }
+
+    public void setTimeSinceLastCollision(float timeSinceLastCollision) {
+        this.timeSinceLastCollision = timeSinceLastCollision;
+    }
+
+    public float getMinTimeBetweenCollision() {
+        return minTimeBetweenCollision;
+    }
+
+    public void setMinTimeBetweenCollision(float minTimeBetweenCollision) {
+        this.minTimeBetweenCollision = minTimeBetweenCollision;
+    }
+    
+    
+    
+    
     
 }
