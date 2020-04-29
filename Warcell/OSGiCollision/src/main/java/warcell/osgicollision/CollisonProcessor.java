2359a40679ea9e476d66074f068a6304de73ae56
Jones_lund <42931029+Coachmeister@users.noreply.github.com>
2020-04-29 12:35:51 +0200

    something something

---
+++ Warcell/OSGiCollision/src/main/java/warcell/osgicollision/CollisonProcessor.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package warcell.osgicollision;
+
+import warcell.common.data.GameData;
+import warcell.common.data.World;
+import warcell.common.services.IPostEntityProcessingService;
+
+
+/**
+ *
+ * @author Jonas
+ */
+public class CollisonProcessor implements IPostEntityProcessingService {
+
+    @Override
+    public void process(GameData gameData, World world) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+
+   
+    }
+
+
