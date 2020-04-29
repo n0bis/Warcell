1e041eede69988b18ba0f12079d3374d6438d563
Mathiask1 <birkebjerg99@live.dk>
2020-04-29 11:42:40 +0200

    Update RifleWeapon.java

--- Warcell/OSGiWeapon/src/main/java/warcell/weapon/rifle/RifleWeapon.java
+++ Warcell/OSGiWeapon/src/main/java/warcell/weapon/rifle/RifleWeapon.java
@@ -22,12 +22,10 @@ import warcell.common.weapon.service.WeaponsSPI;
  *
  * @author birke
  */
-public class RifleWeapon implements WeaponsSPI, BulletSPI {
+public class RifleWeapon implements WeaponsSPI {
     private final String name = "Colt M4A1";
     private final String description = "Automatic Carbine";
     private final String iconPath = "";
-    private BulletSPI bulletService;
-
     
     @Override
     public String getName() {
@@ -43,50 +41,10 @@ public class RifleWeapon implements WeaponsSPI, BulletSPI {
     public String getIconPath() {
         return iconPath;
     }
-
-    @Override
-    public void shoot(Entity shooter, GameData gameData, World world) {
-        Entity bullet = createBullet(shooter, gameData);
-        world.addEntity(bullet);
-    }
-
+ 
     
-    public void setBulletService(BulletSPI bulletService) {
-        this.bulletService = bulletService;
-    }
-
-    public void removeBulletService() {
-        this.bulletService = null;
-    }
-        
-    @Override
-    public Entity createBullet(Entity e, GameData gameData) {
-        PositionPart shooterPos = e.getPart(PositionPart.class);
-
-        float x = shooterPos.getX();
-        float y = shooterPos.getY();
-        float radians = shooterPos.getRadians();
-        float speed = 350;
-
-        Entity bullet = new Bullet();
-        bullet.setRadius(2);
-
-        float bx = (float) cos(radians) * e.getRadius() * bullet.getRadius();
-        float by = (float) sin(radians) * e.getRadius() * bullet.getRadius();
-        
-
-        bullet.add(new PositionPart(bx + x, by + y, radians));
-        bullet.add(new LifePart(1));
-        bullet.add(new MovingPart(0, 5000000, speed, 5));
-        bullet.add(new TimerPart(1));
-
-        bullet.setShapeX(new float[2]);
-        bullet.setShapeY(new float[2]);
-
-        return bullet;    
-    }
     
-        /*    @Override
+    @Override
     public void shoot(Entity shooter, GameData gd, World world) {
     System.out.println("rifle pew");
     PositionPart shooterPos = shooter.getPart(PositionPart.class);
@@ -110,5 +68,5 @@ public class RifleWeapon implements WeaponsSPI, BulletSPI {
     bullet.setShapeY(new float[2]);
     
     world.addEntity(bullet);
-    }*/
+    }
 }
