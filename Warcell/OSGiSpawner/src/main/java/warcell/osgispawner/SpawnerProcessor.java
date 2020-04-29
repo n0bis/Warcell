24612763fd9b6438cb131efb3fca2c2758194837
Patrick <prik.n@hotmail.de>
2020-04-29 22:15:17 +0200

    Now spawns around the spawner
    again, read the title

--- Warcell/OSGiSpawner/src/main/java/warcell/osgispawner/SpawnerProcessor.java
+++ Warcell/OSGiSpawner/src/main/java/warcell/osgispawner/SpawnerProcessor.java
@@ -1,5 +1,6 @@
 package warcell.osgispawner;
 
+import java.util.Random;
 import warcell.common.data.Entity;
 import warcell.common.data.GameData;
 import warcell.common.data.World;
@@ -11,6 +12,8 @@ import warcell.common.services.IGamePluginService;
 
 public class SpawnerProcessor implements IEntityProcessingService {
 
+    Random r = new Random();
+    
     @Override
     public void process(GameData gameData, World world) {
 
@@ -24,8 +27,10 @@ public class SpawnerProcessor implements IEntityProcessingService {
                         while (world.getEntities(Enemy.class).size() < 5) {
                             plugin.start(gameData, world);
                             PositionPart ppE = world.getEntities(Enemy.class).get(world.getEntities(Enemy.class).size()-1).getPart(PositionPart.class);
-                            ppE.setX(positionPart.getX());
-                            ppE.setY(positionPart.getY());
+                            ppE.setX(positionPart.getX() + r.nextInt(100)-50);
+                            System.out.println("X: " + ppE.getX());
+                            ppE.setY(positionPart.getY() + r.nextInt(100)-50);
+                            System.out.println("Y: " + ppE.getY());
                         }
                     }
                 }
