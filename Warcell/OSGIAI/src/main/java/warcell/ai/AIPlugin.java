41d8b35c00c6da7380fd01990dc0fc3f3c8c9927
n0bis <madsfalken@Madss-MacBook-Pro.local>
2020-05-11 13:16:45 +0200

    now with errors

--- Warcell/OSGIAI/src/main/java/warcell/ai/AIPlugin.java
+++ Warcell/OSGIAI/src/main/java/warcell/ai/AIPlugin.java
@@ -18,6 +18,8 @@ import org.xguzm.pathfinding.grid.GridCell;
 import org.xguzm.pathfinding.grid.finders.AStarGridFinder;
 import org.xguzm.pathfinding.grid.finders.GridFinderOptions;
 import org.xguzm.pathfinding.grid.heuristics.ManhattanDistance;
+import warcell.astar.AStar;
+import warcell.astar.AStarNode;
 import warcell.common.ai.AISPI;
 import warcell.common.data.entityparts.PositionPart;
 import warcell.common.data.entityparts.TiledMapPart;
@@ -28,47 +30,54 @@ import warcell.common.data.entityparts.TiledMapPart;
  */
 public class AIPlugin implements AISPI {
     
-    Map map;
-    NavigationTiledMapLayer navigationLayer;
-    AStarGridFinder<GridCell> finder = new AStarGridFinder(GridCell.class);
+    private AStar ai;
+    private AStarNode[][] nodes;
+    private Map map;
+    private NavigationTiledMapLayer navigationLayer;
+    private AStarGridFinder<GridCell> finder = new AStarGridFinder(GridCell.class);
     
     @Override
     public void startAI(TiledMapPart tiledMap) {
-        
         if (map == null) {
-        map = new NavTmxMapLoader().load(tiledMap.getSrcPath());
-        navigationLayer = (NavigationTiledMapLayer) map.getLayers().get("navigation"); 
+            map = new NavTmxMapLoader().load(tiledMap.getSrcPath());
+            navigationLayer = (NavigationTiledMapLayer) map.getLayers().get("navigation");
+            int rows = navigationLayer.getHeight();
+            int columns = navigationLayer.getWidth();
+            
+            nodes = new AStarNode[rows][columns];
+            
+            GridCell[][] gridCells = navigationLayer.getNodes();
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    GridCell gridCell = gridCells[i][j];
+                    AStarNode node = new AStarNode(gridCell.getX(), gridCell.getY());
+                    node.setW(gridCell.isWalkable() ? 1000 : 0);
+                    node.setBlock(gridCell.isWalkable());
+                    nodes[i][j] = node;
+                }
+            }
+            
+            ai = new AStar(rows, columns);
+            ai.setSearchArea(nodes);
         }
     }
 
     @Override
-    public List<PositionPart> getPath(PositionPart source, PositionPart target) {
-
-        int sourceX = Math.round(source.getX()) / 32;
-        int sourceY = Math.round(source.getY()) / 32;
+    public List<PositionPart> getPath(PositionPart sourcePart, PositionPart targetPart) {
         
-        int targetX = Math.round(target.getX()) / 32;
-        int targetY = Math.round(target.getY()) / 32;
+        AStarNode source = new AStarNode(Math.round(sourcePart.getX()), Math.round(sourcePart.getY()));
+        AStarNode target = new AStarNode(Math.round(targetPart.getX()), Math.round(targetPart.getY()));
+        ai.setSourceAndTargetNode(source, target);
         
-        //System.out.println("sourceX: " + sourceX + " sourceY: " + sourceY);
-        //System.out.println("targetX: " + targetX + " targetY: " + targetY);
-        List<GridCell> thePath = new ArrayList<>();
-        try {
-            thePath = finder.findPath(sourceX, sourceY, targetX, targetY, navigationLayer);
-        } catch (PathFindingException e) {
-            return new ArrayList<>();
-        }
-        
-        if(thePath == null)
-            return new ArrayList<>();
-        
-        List<PositionPart> pathToEnd = new ArrayList<>();
-        
-        for (GridCell path : thePath) {
-            pathToEnd.add(new PositionPart(path.getX() * 32, path.getY() * 32, 0));
-        }
-        
-        return pathToEnd;
+        //Gets path from AStar
+        ArrayList<AStarNode> nodePath = (ArrayList<AStarNode>) ai.findPath();
+        //List to be populated and returned
+        ArrayList<PositionPart> path = new ArrayList<>();
+ 
+        nodePath.forEach((node) -> {
+            path.add(new PositionPart(node.getRow(), node.getCol(), 0));
+        });
+        return path;
     }
 
 }
