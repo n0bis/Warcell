bc5b7bd149925b778f4801859b0f9ca077bef889
Patrick <prik.n@hotmail.de>
2020-05-17 21:02:21 +0200

    Not anymore

--- Warcell/OSGiCollision/src/main/java/warcell/osgicollision/CollisionProcessor.java
+++ Warcell/OSGiCollision/src/main/java/warcell/osgicollision/CollisionProcessor.java
@@ -53,87 +53,89 @@ public class CollisionProcessor implements IPostEntityProcessingService {
                 objects = map.getLayers().get("objectlayer").getObjects();
                 border = map.getLayers().get("border").getObjects();
             }
-        } else {
-            for (Entity entity1 : world.getEntities()) {
-                for (Entity entity2 : world.getEntities(Enemy.class)) {
-                    if (!entity1.equals(entity2)) {
-                        if (hasCollided(entity1, entity2)) {
-                            CollisionPart collisionPart1 = entity1.getPart(CollisionPart.class);
-                            CollisionPart collisionPart2 = entity2.getPart(CollisionPart.class);
-                            LifePart lp1 = entity1.getPart(LifePart.class);
-                            LifePart lp2 = entity2.getPart(LifePart.class);
-                            DamagePart dp1 = entity1.getPart(DamagePart.class);
-                            DamagePart dp2 = entity2.getPart(DamagePart.class);
+        }
+        for (Entity entity1 : world.getEntities()) {
+            for (Entity entity2 : world.getEntities(Enemy.class)) {
+                if (!entity1.equals(entity2)) {
+                    if (hasCollided(entity1, entity2)) {
+                        CollisionPart collisionPart1 = entity1.getPart(CollisionPart.class);
+                        CollisionPart collisionPart2 = entity2.getPart(CollisionPart.class);
+                        LifePart lp1 = entity1.getPart(LifePart.class);
+                        LifePart lp2 = entity2.getPart(LifePart.class);
+                        DamagePart dp1 = entity1.getPart(DamagePart.class);
+                        DamagePart dp2 = entity2.getPart(DamagePart.class);
 
-                            if (timer1 > collisionPart1.getMinTimeBetweenCollision()) {
-                                lp2.takeDamage(dp1.getDamage());
-                                timer1 = 0;
-                            }
-
-                            if (timer2 > collisionPart2.getMinTimeBetweenCollision()) {
-                                lp1.takeDamage(dp2.getDamage());
-                            }
+                        if (timer1 > collisionPart1.getMinTimeBetweenCollision()) {
+                            lp2.takeDamage(dp1.getDamage());
+                            timer1 = 0;
                         }
-                        timer1 += gameData.getDelta();
-                        timer2 += gameData.getDelta();
-                    }
-                }
-            }
 
-            for (Entity player : world.getEntities(Player.class)) {
-                MovingPart movingPart = player.getPart(MovingPart.class);
-                PositionPart positionPart = player.getPart(PositionPart.class);
-                
-                for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
-                    Rectangle rectangle = rectangleObject.getRectangle();
-                    Rectangle playerRectangle = new Rectangle(positionPart.getX(), positionPart.getY(), 35, 35);
-                    
-                    if (Intersector.overlaps(rectangle, playerRectangle)) {
-                        movingPart.setIsInWalls(true);
+                        if (timer2 > collisionPart2.getMinTimeBetweenCollision()) {
+                            lp1.takeDamage(dp2.getDamage());
+                        }
                     }
-                }
-                for (RectangleMapObject rectangleObject : border.getByType(RectangleMapObject.class)) {
-                    Rectangle rectangle = rectangleObject.getRectangle();
-                    Rectangle playerRectangle = new Rectangle(positionPart.getX(), positionPart.getY(), 35, 35);
-                    
-                    if (Intersector.overlaps(rectangle, playerRectangle)) {
-                        movingPart.setIsInWalls(true);
-                    }
-                }
-                if (!movingPart.isIsInWalls()) {
-                    movingPart.setLastX(positionPart.getX());
-                    movingPart.setLastY(positionPart.getY());
-                }
-            }
-            for (Entity bullet : world.getEntities(Bullet.class)) {
-                BulletMovingPart movingPart = bullet.getPart(BulletMovingPart.class);
-                PositionPart positionPart = bullet.getPart(PositionPart.class);
-                
-                for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
-                    Rectangle rectangle = rectangleObject.getRectangle();
-                    Rectangle playerRectangle = new Rectangle(positionPart.getX(), positionPart.getY(), 5, 5);
-                    
-                    if (Intersector.overlaps(rectangle, playerRectangle)) {
-                        world.removeEntity(bullet);
-                        
-                    }
+                    timer1 += gameData.getDelta();
+                    timer2 += gameData.getDelta();
                 }
             }
         }
 
+        for (Entity player : world.getEntities(Player.class)) {
+            MovingPart movingPart = player.getPart(MovingPart.class);
+            PositionPart positionPart = player.getPart(PositionPart.class);
+
+            for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
+                Rectangle rectangle = rectangleObject.getRectangle();
+                Rectangle playerRectangle = new Rectangle(positionPart.getX(), positionPart.getY(), 35, 35);
+
+                if (Intersector.overlaps(rectangle, playerRectangle)) {
+                    movingPart.setIsInWalls(true);
+                }
+            }
+            for (RectangleMapObject rectangleObject : border.getByType(RectangleMapObject.class)) {
+                Rectangle rectangle = rectangleObject.getRectangle();
+                Rectangle playerRectangle = new Rectangle(positionPart.getX(), positionPart.getY(), 35, 35);
+
+                if (Intersector.overlaps(rectangle, playerRectangle)) {
+                    movingPart.setIsInWalls(true);
+                }
+            }
+            if (!movingPart.isIsInWalls()) {
+                movingPart.setLastX(positionPart.getX());
+                movingPart.setLastY(positionPart.getY());
+            }
+        }
+        for (Entity bullet : world.getEntities(Bullet.class)) {
+            BulletMovingPart movingPart = bullet.getPart(BulletMovingPart.class);
+            PositionPart positionPart = bullet.getPart(PositionPart.class);
+
+            for (RectangleMapObject rectangleObject : objects.getByType(RectangleMapObject.class)) {
+                Rectangle rectangle = rectangleObject.getRectangle();
+                Rectangle playerRectangle = new Rectangle(positionPart.getX(), positionPart.getY(), 5, 5);
+
+                if (Intersector.overlaps(rectangle, playerRectangle)) {
+                    world.removeEntity(bullet);
+
+                }
+            }
+        }
     }
 
     private boolean hasCollided(Entity entity1, Entity entity2) {
-        SquarePart squarePart1 = entity1.getPart(SquarePart.class);
+        SquarePart squarePart1 = entity1.getPart(SquarePart.class
+        );
         SquarePart squarePart2 = entity2.getPart(SquarePart.class);
 
         //no collision if no circle part
-        if (squarePart1 == null || squarePart2 == null || entity1.getClass().equals(entity2.getClass())) {
+        if (squarePart1 == null || squarePart2
+                == null || entity1.getClass()
+                        .equals(entity2.getClass())) {
             return false;
         }
         float distX = squarePart1.getCentreX() - squarePart2.getCentreX();
         float distY = squarePart1.getCentreY() - squarePart2.getCentreY();
         float distance = (float) (Math.sqrt(distX * distX + distY * distY));
+
         return distance < (squarePart1.getRadius() + squarePart2.getRadius());
 
     }
