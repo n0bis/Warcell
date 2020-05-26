9c726f6e426a1ab8226ef7ef74c192822c87f783
Patrick Nielsen <prik.n@hotmail.de>
2020-05-26 20:29:45 +0200

    new branch of slow and fast enemies that will hunt you down if youre not fast enough version 2.0 (#33)
    It's a new branch of slow and fast enemies that will hunt you down if youre not fast enough version 2.0

--- Warcell/OSGiEnemy/src/main/java/warcell/osgienemy/EnemyPlugin.java
+++ Warcell/OSGiEnemy/src/main/java/warcell/osgienemy/EnemyPlugin.java
@@ -1,5 +1,6 @@
 package warcell.osgienemy;
 
+import java.util.Random;
 import warcell.common.data.Entity;
 import warcell.common.data.GameData;
 import warcell.common.data.World;
@@ -21,6 +22,7 @@ public class EnemyPlugin implements IGamePluginService {
     private final int walkAnimationFrameColumns = 17;
     private final int walkAnimationFrameRows = 1;
     private int amountOfEnemies;
+    private Random random = new Random();
 
     public EnemyPlugin() {
         amountOfEnemies = 1;
@@ -53,7 +55,7 @@ public class EnemyPlugin implements IGamePluginService {
         enemyZombie.add(new CirclePart(centreX, centreY, enemyRadius));
         enemyZombie.add(new LifePart(maxLife));
         enemyZombie.add(new DamagePart(2, 3));
-        enemyZombie.add(new MovingPart(acceleration, maxSpeed, true));
+        enemyZombie.add(new MovingPart(acceleration + acceleration * random.nextFloat(), maxSpeed + maxSpeed * random.nextFloat(), true));
         enemyZombie.add(new PositionPart(x, y, radians));
         enemyZombie.add(new CollisionPart(true, 3));
         enemyZombie.add(new AnimationTexturePart(new Vector2D(x, y), walkAnimationPath, walkAnimationFrameColumns, walkAnimationFrameRows, 0.09f, 56f, 63f, 1f, 1f));
